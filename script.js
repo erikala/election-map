7d30bffece1eb7b2566cf62459a227d5ef08b77d
Erika La <henriikka.laurola@gmail.com>
2019-03-19 22:41:42 +0100

    added party color as a parameter

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var createPolitician = function(name)
+var createPolitician = function(name, partyColor)
 {
 
 var politician = {};
 
 politician.name = name;
+
+politician.partyColor = partyColor;
 
 politician.electionResults = null;
 
@@ -22,8 +24,8 @@ politician.tallyUpTotalVotes = function(){
 return politician;
 };
 
-var jane = createPolitician("Jane Doesitall");
-var betsy = createPolitician("Betsy Rocks");
+var jane = createPolitician("Jane Doesitall", [132, 17, 11]);
+var betsy = createPolitician("Betsy Rocks", [245, 141, 136]);
 
 jane.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 betsy.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
@@ -60,3 +62,6 @@ else{
 }
 
 console.log("And the winner is..." + winner + "!");
+
+console.log("Betsy's color is: " + betsy.partyColor);
+console.log("Jane's color is: " + jane.partyColor);
